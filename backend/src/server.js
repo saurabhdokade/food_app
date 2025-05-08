ba42cd26222a43a7936da60734562d3729e416d0
saurabh eknath dokade <saurabhdokade77@gmail.com>
2025-05-08 13:39:04 +0530

    .

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,42 +1,7 @@
-import dotenv from 'dotenv';
-dotenv.config();
-import { fileURLToPath } from 'url';
-import express from 'express';
-import cors from 'cors';
-import foodRouter from './routers/food.router.js';
-import userRouter from './routers/user.router.js';
-import orderRouter from './routers/order.router.js';
-import uploadRouter from './routers/upload.router.js';
+import app from './app.js';
 
-import { dbconnect } from './config/database.config.js';
-import path, { dirname } from 'path';
-dbconnect();
+const PORT = process.env.PORT || 5000;
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-const app = express();
-app.use(express.json());
-app.use(
-  cors({
-    credentials: true,
-    origin: ['https://food-app-wtkv.vercel.app/'],  })
-);
-
-app.use('/api/foods', foodRouter);
-app.use('/api/users', userRouter);
-app.use('/api/orders', orderRouter);
-app.use('/api/upload', uploadRouter);
-
-const publicFolder = path.join(__dirname, 'public');
-app.use(express.static(publicFolder));
-
-app.get('*', (req, res) => {
-  const indexFilePath = path.join(publicFolder, 'index.html');
-  res.sendFile(indexFilePath);
-});
-
-const PORT = process.env.PORT;
 app.listen(PORT, () => {
-  console.log('listening on port ' + PORT);
+  console.log(`Server running on http://localhost:${PORT}`);
 });
