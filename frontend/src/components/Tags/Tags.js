93a7ceb602d04ed5485dec0151a367b85a8307f6
saurabh eknath dokade <saurabhdokade77@gmail.com>
2025-05-08 16:23:14 +0530

    .

--- frontend/src/components/Tags/Tags.js
+++ frontend/src/components/Tags/Tags.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import classes from './tags.module.css';
+const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || "https://food-app-ijkw.vercel.app";
 
 export default function Tags({ tags, forFoodPage }) {
   return (
@@ -11,7 +12,7 @@ export default function Tags({ tags, forFoodPage }) {
       }}
     >
       {tags.map(tag => (
-        <Link key={tag.name} to={`/tag/${tag.name}`}>
+        <Link key={tag.name} to={`${API_BASE_URL}/tag/${tag.name}`}>
           {tag.name}
           {!forFoodPage && `(${tag.count})`}
         </Link>
