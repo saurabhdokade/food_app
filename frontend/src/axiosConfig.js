1c903e8a3f210278d1ee29c2fda6e4a7d146afb5
saurabh eknath dokade <saurabhdokade77@gmail.com>
2025-05-08 16:15:59 +0530

    .

--- frontend/src/axiosConfig.js
+++ frontend/src/axiosConfig.js
@@ -1,4 +1,4 @@
-import axios from 'axios';
+// import axios from 'axios';
 
-axios.defaults.baseURL =
-  process.env.NODE_ENV !== 'production' ? 'https://food-app-ijkw.vercel.app' : '';
+// axios.defaults.baseURL =
+//   process.env.NODE_ENV !== 'production' ? 'https://food-app-ijkw.vercel.app' : '/';
